70099976ce4f503f68052bccc114a40134801ce3
Hi <Hi@DESKTOP-4ENKVOC>
2024-07-07 18:29:16 +0530

    First commit

---
+++ p2/src/p2/abc.java
@@ -0,0 +1,21 @@
+package p2;
+
+public class abc {
+	
+	 
+	 public static void main(String[] args)
+	 {
+	 System.out.println("\n Prog. is showing even no");
+	 for(int i=2;i<=20;i++)
+	 {
+	 if(i%2==0)
+	 {
+	 System.out.print("\n "+i);
+	 }
+
+	 }
+	 }
+	} 
+
+
+	
