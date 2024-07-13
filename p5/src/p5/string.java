deb990eca0d6f98d2e5d2979febfc8898ee131fc
Hi <Hi@DESKTOP-4ENKVOC>
2024-07-13 16:25:03 +0530

    FIRST COMMIT

---
+++ p5/src/p5/string.java
@@ -0,0 +1,10 @@
+package p5;
+
+public class string {
+public static void main (String args[]) {
+	String s1 = new String("C2TC");
+	String s2 = new String("C2TC");
+	System.out.println(s1 == s2 );
+	System.out.println(s1.equals(s2));
+}
+}
